162f45e392e5f1d146af03a0d466139205d85a7f
Jiaxuan <e1124042@u.nus.edu>
2024-07-12 17:19:48 +0800

    Add learn_git.R file

---
+++ R/learn_git.R
@@ -0,0 +1,16 @@
+#how to use git# 在terminal中
+
+git status #check
+
+#add new file, for example 0319.docx#
+git add 0319.docx
+
+#提交更改#
+git commit -m "描述你添加的文件或做出的更改"
+
+#推送更改到远程仓库#
+git push
+
+
+git config --global user.email "e1124042@u.nus.edu"
+git config --global user.name "Jiaxuan"
